b1391f044b53b8c1414b41e47ea14ec3a4a5cd1a
Amr69130 <amrouchenorris@gmail.com>
2025-03-10 11:51:55 +0100

    delete form wip

--- exo_voitures/delete.php
+++ exo_voitures/delete.php
@@ -1,4 +1,49 @@
 <?php
-$pdo = connectDB();
+//verif presence ID url
+var_dump($_GET['id']);
 
+
+require('connectDB.php');
+
+
+//Recup en bdd des données grâce à leur ID
+$pdo = connectDB();
+$requete = $pdo->prepare("SELECT * FROM car WHERE id = :id;");
+$requete->execute([
+    'id' => $_GET['id']
+]);
+
+$car = $requete->fetch();
+//verif  si resultat
+var_dump($car);
+if ($car === false) {
+
+    header('location: index.php');
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+
+
+    $requestDelete = $pdo->prepare("DELETE FROM car WHERE id = :id;");
+    $requestDelete->execute(
+        [
+            ":id" => $car['id']
+        ]
+    );
+
+}
+
+
+?>
+
+<?php
+include('header.php');
+?>
+<form action="POST" action="delete.php?id=<?= $_GET['id'] ?>">
+    <button class="btn btn-danger">SUPPRIMER</button>
+    <button class="btn btn-secondary" formaction="index.php">ANNULER</button>
+</form>
+<?php
+include('footer.php');
 ?>
