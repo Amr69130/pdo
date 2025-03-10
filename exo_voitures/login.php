18437f5cd5690000892ecf3c14f7f8c188c17c38
Amr69130 <amrouchenorris@gmail.com>
2025-03-10 13:31:45 +0100

    login form wip

--- exo_voitures/login.php
+++ exo_voitures/login.php
@@ -1,4 +1,14 @@
 <?php
+require('connectDB.php');
 $pdo = connectDB();
+$pass = password_hash("admin", PASSWORD_DEFAULT);
+var_dump($pass)
+    ?>
 
-?>
+<form method="POST" action="login.php">
+    <label>Username</label>
+    <input required type="text" name="username">
+    <label>Password</label>
+    <input required type="password" name="password">
+    <button class="btn btn-outline-succes">Se connecter</button>
+</form>
